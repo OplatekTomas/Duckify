3c849eb4d438fa2fae5674b6f3125b829671e9c3
Tomáš Oplatek <tomas.oplatek@gmail.com>
2019-03-10 23:05:41 +0100

    Changes on player
    getting the player to run.. its progress but it aint much

--- Duckify/wwwroot/js/player.js
+++ Duckify/wwwroot/js/player.js
@@ -3,13 +3,18 @@
     return b ? b.pop() : '';
 }
 
+
+var player;
+var api;
+const playerName = 'Duckify Web Playback'
 window.onSpotifyWebPlaybackSDKReady = () => {
     const token = spotifyToken;
-    const player = new Spotify.Player({
-        name: 'Duckify Web Playback',
+    player = new Spotify.Player({
+        name: playerName,
         getOAuthToken: cb => { cb(token); }
     });
-
+    api = new SpotifyWebApi();
+    api.setAccessToken(token);
     // Error handling
     player.addListener('initialization_error', ({ message }) => { console.error(message); });
     player.addListener('authentication_error', ({ message }) => { console.error(message); });
@@ -21,7 +26,8 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 
     // Ready
     player.addListener('ready', ({ device_id }) => {
-        console.log('Ready with Device ID', device_id);
+        transferPlayback(device_id);
+        //console.log('Ready with Device ID', device_id);
     });
 
     // Not Ready
@@ -32,3 +38,20 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     // Connect to the player!
     player.connect();
 };
+
+function transferPlayback(device_id) {
+    var array = [device_id];
+    api.transferMyPlayback(array, null, function (success, data) {
+        console.log(success)
+        if (success == null) {
+            window.setInterval(function () {
+                player.getCurrentState().then(data => renderUI(data));
+            }, 1000);
+        }
+    });
+}
+
+function renderUI(data) {
+    $("#playerImageCover").attr("src", data.track_window.current_track.album.images[0].url)
+
+}
