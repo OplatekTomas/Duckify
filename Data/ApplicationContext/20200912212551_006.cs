4eee00da4339cb3c5f0ea664642e0c58c0630038
Kaharonus <tomas.oplatek@gmail.com>
2020-09-12 23:28:37 +0200

    Fixed login, added temp page, played around with ui

---
+++ Data/ApplicationContext/20200912212551_006.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Duckify.Data.ApplicationContext
+{
+    public partial class _006 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTemp",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTemp",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Identity;
 
 namespace Duckify.Models {
     public class User : IdentityUser {
         public List<UserStarredSongs> FavoriteSongs { get; set; }
 
         public string AccessCode { get; set; }
+
+        public bool IsTemp { get; set; } = false;
 
         public DateTime ValidUntil { get; set; }
     }
